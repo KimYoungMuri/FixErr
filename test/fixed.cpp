83a62a8702a026637b66fdcac48061d4eb136644
KimYoungMuri <71124809+KimYoungMuri@users.noreply.github.com>
2021-12-28 11:06:22 +0900

    Add files via upload

---
+++ test/fixed.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+int main ( ) {
+int a , i = 0 , b = 0 ;
+scanf ( " %d " , & a ) ;
+while ( i < a ) {
+int j ;
+scanf ( " %d " , & j ) ;
+if ( j < 2 ) {
+j ++ ;
+}
+else if ( j % 2 == 0 ) {
+b = b + 1 ;
+i ++ ;
+}
+else {
+i ++ ;
+}
+}
+printf ( " %d " , b ) ;
+return 0 ;
+}
